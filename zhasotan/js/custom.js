abbdd898e2b7ec805bd024a2f8092e4f291677be
followdes <chingizdk@gmail.com>
2017-07-01 16:58:43 +0600

    v1.2
    <>

--- zhasotan/js/custom.js
+++ zhasotan/js/custom.js
@@ -1,13 +1,106 @@
-$(document).ready(function() {
-    $('a[href="#navbar-more-show"], .navbar-more-overlay').on('click', function(event) {
-		event.preventDefault();
-		$('body').toggleClass('navbar-more-show');
-		if ($('body').hasClass('navbar-more-show'))	{
-			$('a[href="#navbar-more-show"]').closest('li').addClass('active');
-		}
-    else{
-			$('a[href="#navbar-more-show"]').closest('li').removeClass('active');
-		}
-		return false;
-	});
+$(document).ready(function(){
+
+/**
+ * This object controls the nav bar. Implement the add and remove
+ * action over the elements of the nav bar that we want to change.
+ *
+ * @type {{flagAdd: boolean, elements: string[], add: Function, remove: Function}}
+ */
+var myNavBar = {
+
+    flagAdd: true,
+
+    elements: [],
+
+    init: function (elements) {
+        this.elements = elements;
+    },
+
+    add : function() {
+        if(this.flagAdd) {
+            for(var i=0; i < this.elements.length; i++) {
+                document.getElementById(this.elements[i]).className += " fixed-theme";
+            }
+            this.flagAdd = false;
+        }
+    },
+
+    remove: function() {
+        for(var i=0; i < this.elements.length; i++) {
+            document.getElementById(this.elements[i]).className =
+                    document.getElementById(this.elements[i]).className.replace( /(?:^|\s)fixed-theme(?!\S)/g , '' );
+        }
+        this.flagAdd = true;
+    }
+
+};
+
+/**
+ * Init the object. Pass the object the array of elements
+ * that we want to change when the scroll goes down
+ */
+myNavBar.init(  [
+    "header",
+    "header-container",
+    "brand"
+]);
+
+/**
+ * Function that manage the direction
+ * of the scroll
+ */
+function offSetManager(){
+
+    var yOffset = 0;
+    var currYOffSet = window.pageYOffset;
+
+    if(yOffset < currYOffSet) {
+        myNavBar.add();
+    }
+    else if(currYOffSet == yOffset){
+        myNavBar.remove();
+    }
+
+}
+
+/**
+ * bind to the document scroll detection
+ */
+window.onscroll = function(e) {
+    offSetManager();
+}
+
+/**
+ * We have to do a first detectation of offset because the page
+ * could be load with scroll down set.
+ */
+offSetManager();
+
+var clickEvent = false;
+$('#myCarousel').carousel({
+  interval:   4000
+}).on('click', '.list-group li', function() {
+    clickEvent = true;
+    $('.list-group li').removeClass('active');
+    $(this).addClass('active');
+}).on('slid.bs.carousel', function(e) {
+  if(!clickEvent) {
+    var count = $('.list-group').children().length -1;
+    var current = $('.list-group li.active');
+    current.removeClass('active').next().addClass('active');
+    var id = parseInt(current.data('slide-to'));
+    if(count == id) {
+      $('.list-group li').first().addClass('active');
+    }
+  }
+  clickEvent = false;
+});
+
+});
+
+$(window).load(function() {
+    var boxheight = $('#myCarousel .carousel-inner').innerHeight();
+    var itemlength = $('#myCarousel .item').length;
+    var triggerheight = Math.round(boxheight/itemlength+1);
+	$('#myCarousel .list-group-item').outerHeight(triggerheight);
 });
